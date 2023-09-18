2189780b0aafbb6769bb761da42fa702e127395c
Kahnu Charan Swain <djkanha044@gmail.com>
2023-09-18 10:44:53 +0530

    Initial Commit

---
+++ wp-menu-by-user-roles.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: WP Menu By User roles
+Description: It empowers website administrators to create custom menus tailored to specific user roles
+Author: kahnu044
+Version: 1.0
+*/
+
